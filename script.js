fdae7328a74b1a30ab53fa5e48d5d5d439cab7fd
sathya14Dev <sathyabintang@gmail.com>
2024-09-11 20:50:03 +0800

    firstcommit

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 let images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4xa9bt8CaM5b7lpu6H4M09bYMsCwAe-DXnQ&s", 
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfj1Mltc-92Ll44n0XTIWxDYgWcGzlTMHtlQ&s",
                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtEFQgXgxPmJU0tsxtHC6WZ_4Ej3dravbjiw&s"];
-let songs = ["JKT48 - Ingatan Kosmos (Cosmos no Kioku) Color Coded Lyrics.mp3", 
+let music = ["JKT48 - Ingatan Kosmos (Cosmos no Kioku) Color Coded Lyrics.mp3", 
                "Eureka Milik Kita.mp3",
-               "HEAVY ROTATION - JKT48 (Liriik Video).mp3"]; // 
-        let currentIndex = 0; //
+               "HEAVY ROTATION - JKT48 (Liriik Video).mp3"]; 
+        let currentIndex = 0; 
 
         function changeSong() {
             currentIndex = (currentIndex + 1) % images.length;
             
-            let albumArt = document.getElementById('albumArt');
-            albumArt.src = images[currentIndex];
+            let image = document.getElementById("gambar");
+            image.src = images[currentIndex];
 
-            let audioPlayer = document.getElementById('audioPlayer');
-            let audioSource = document.getElementById('audioSource');
-            audioSource.src = songs[currentIndex];
+            let musick = document.getElementById("musik");
+            musick.src = music[currentIndex];
             
-            audioPlayer.load();
+            musick.load();
         }
